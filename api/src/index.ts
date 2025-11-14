2ac1fab0c903e1b9762234df27d373f7127a402a
Alexey Bukharin <alesh-buk@yandex.ru>
2025-11-15 00:42:21 +0300

    fix: Исправил порты

--- api/src/index.ts
+++ api/src/index.ts
@@ -12,7 +12,7 @@ import { ErrorMiddleware } from './middleware/error.middleware';
 import { swaggerSpec } from './config/swagger';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // Middleware
 app.use(helmet());
